a7a48e0b70d9434880dda9a1aee5499fab030ebc
ekarlson238 <ekarlson238@gmail.com>
2019-03-07 15:15:12 -0600

    interactive objects part 4

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    private AudioSource audioSource;
+
     [SerializeField]
     private string displayText = "Interactive Object";
     public string DisplayText
@@ -14,8 +17,22 @@ public class InteractiveObject : MonoBehaviour, IInteractive
         }
     }
 
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void interactWith()
     {
+        try
+        {
+            audioSource.Play();
+        }
+        catch (System.Exception)
+        {
+            throw new System.Exception("Interactive Object requires an Audio Source");
+        }
+
         Debug.Log("Player just interacted with " + gameObject.name);
     }
 }
