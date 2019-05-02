cfdeff67b7ef9dfedb048da4edaae7dd9a6b50b5
ekarlson238 <ekarlson238@gmail.com>
2019-05-02 16:41:54 -0500

    Started scene management scripts

---
+++ Assets/Scripts/SceneValueManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneValueManager : MonoBehaviour
+{
+    public static bool keyPickedUp = false;
+    public static bool keyUsedDoorOpen = false;
+
+    public static bool item1PickedUp = false;
+    public static bool item1InInventory = false;
+    public static bool item1Placed = false;
+
+    public static bool item2PickedUp = false;
+    public static bool item2InInventory = false;
+    public static bool item2Placed = false;
+
+    public static bool item3PickedUp = false;
+    public static bool item3InInventory = false;
+    public static bool item3Placed = false;
+
+    public static bool item4PickedUp = false;
+    public static bool item4InInventory = false;
+    public static bool item4Placed = false;
+
+    public static bool note1PickedUp = false;
+    public static bool note2PickedUp = false;
+    public static bool note3PickedUp = false;
+    public static bool note4PickedUp = false;
+
+}
