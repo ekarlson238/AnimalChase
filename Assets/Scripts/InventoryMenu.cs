95952797bfbafe43fc6b2ff8738391111da374a5
ekarlson238 <ekarlson238@gmail.com>
2019-05-06 21:18:42 -0500

    inventory menu functionality part 3

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+    private CanvasGroup canvasGroup;
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance == null)
+                throw new System.Exception("There is currently no inventory menu instance.");
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    private bool IsVisible => canvasGroup.alpha > 0;
+
+    private void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidbodyFirstPersonController.enabled = true;
+    }
+
+    private void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        rigidbodyFirstPersonController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Show Inventory Menu"))
+        {
+            if (IsVisible)
+                HideMenu();
+            else
+                ShowMenu();
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            throw new System.Exception("There already an instance of inventory menu, there can be only one.");
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        rigidbodyFirstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
+    }
+    private void Start()
+    {
+        HideMenu();
+    }
+}
